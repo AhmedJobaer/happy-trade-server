80ee969b21fad7b531018cb1d644991168424cbb
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-30 03:26:29 +0800

    maongo added

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//VkPm70A9KBNPM5i6
 
-//const uri = "mongodb+srv://<username>:<password>@cluster0.r2o8evu.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.r2o8evu.mongodb.net/?retryWrites=true&w=majority`;
+//const uri = "mongodb://localhost:27017";
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
