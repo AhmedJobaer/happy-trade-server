7cffb6e119e07fa890f23f7020df8bb1b5f27fc8
jobaer-ahmed <ajobaer08@gmail.com>
2022-11-29 17:19:47 +0800

    data dend to user dashboard

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -8,7 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 
 
 //const uri = "mongodb+srv://<username>:<password>@cluster0.r2o8evu.mongodb.net/?retryWrites=true&w=majority";
@@ -38,10 +38,29 @@ async function run() {
         })
 
 
+
+        /* app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollections.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            console.log(user);
+            res.status(403).send({ accessToken: '' })
+        }) */
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             const result = await usersCollections.insertOne(user);
             res.send(result);
+        })
+
+        app.get('/allUser', async (req, res) => {
+            const query = {};
+            const user = await usersCollections.find(query).toArray();
+            res.send(user);
         })
     }
     finally {
@@ -54,4 +73,4 @@ app.get('/', async (req, res) => {
     res.send('happy trade server is running!')
 })
 
-app.listen(port, () => console.log(`Doctors portal running on ${port}`));
+app.listen(port, () => console.log(`happy trade server is running! ${port}`));
